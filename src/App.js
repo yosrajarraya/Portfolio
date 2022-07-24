a364bf31e4fd2f2abd9a0d540c31fec30d3ddbb5
yosra jarraya <yosrajarraya50@gmail.com>
2022-07-24 16:45:23 +0200

    Set up bootstrap and add the Navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 
-
+import Navbar  from "./components/Navbar";
 function App() {
   return (
-    <div className="App">
+    
+    <div>
+      <Navbar/>
       <h1>From App Component</h1>
     </div>
   );
