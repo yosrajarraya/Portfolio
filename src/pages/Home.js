522961556eed11ce713ed6a01ee7495bf9150c3d
yosra jarraya <yosrajarraya50@gmail.com>
2022-07-25 15:51:22 +0200

    Add content to the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import webImage from '../images/photo-2.png'
 function Home() {
   return (
-    <div className="alert alert-info  mt-5 "role="alert">
-Hello My  Name is Yosra , I am a Fullstack Web Developer    </div>
+    <div className="alert alert-info  mt-5  container"role="alert">
+      <img src="/images/photo-1.jpg" width =" 150px" className='width-border-radius'alt="My Photo" />
+<p>Hello My  Name is Yosra , I am a Fullstack Web Developer</p>
+<img src={webImage} alt="Web" style={{width:'100%'}} />
+    </div>
   )
 }
 
